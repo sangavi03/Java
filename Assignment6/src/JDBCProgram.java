d2dadf8498e71a78dc560e89701570bd269a9b45
sangavi03 <64550549+sangavi03@users.noreply.github.com>
2021-03-01 00:09:02 -0600

    Add files via upload

--- Assignment6/src/JDBCProgram.java
+++ Assignment6/src/JDBCProgram.java
@@ -1,7 +1,10 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class JDBCProgram {
 
@@ -16,12 +19,79 @@ public class JDBCProgram {
 			System.out.println("Connection Established");
 		}
 		Statement statement = con.createStatement();
+		//Drop Database
+		String dropdbquery = "DROP DATABASE COLLEGE";
+		int dropdb = statement.executeUpdate(dropdbquery);
+		System.out.println("Database Dropped.");
 		
-		String create = "CREATE TABLE STUDENT (id int(5) primary key, firstname varchar(50),lastname varchar(50), "+
-				 "mobile int(10)),address varchar(255),department varchar(255)";
-
-		System.out.println("Creating table: "+ create);
+		//Creating DB
+		String createDatabase = "CREATE DATABASE COLLEGE";
+		int createdb = statement.executeUpdate(createDatabase);
+		//Creating Table
+		String create = "CREATE TABLE STUDENT (id int(5) primary key, firstname varchar(50),lastname varchar(50), "
+				+ "mobile int(10)),address varchar(255),department varchar(255)";
 		statement.executeUpdate(create);
-	}
+		System.out.println("Table Created");
+		System.out.println("Creating table: " + create);
+		String insert = "insert into STUDENT values(?,?,?,?,?,?)";
+		System.out.println("Inserting Records...");
+		PreparedStatement ps = con.prepareStatement(insert);
+		int record = 0;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the records:");
+		int n = scan.nextInt();
+		for (int i = 1; i <= n; i++) {
+			System.out.println("Enter Id:");
+			int id = scan.nextInt();
+			System.out.println("Enter First Name:");
+			String firstName = scan.next();
+			System.out.println("Enter Last Name:");
+			String lastName = scan.next();
+			System.out.println("Enter Mobile:");
+			int mobile = scan.nextInt();
+			System.out.println("Enter Address:");
+			String location = scan.next();
+			System.out.println("Enter Department:");
+			String department = scan.next();
+			ps.setInt(1, id);
+			ps.setString(2, firstName);
+			ps.setString(3, lastName);
+			ps.setInt(4, mobile);
+			ps.setString(5, location);
+			ps.setString(6, department);
+			record = ps.executeUpdate();
+		}
+		if (record > 0) {
+			System.out.println("Records Inserted");
+		}
+		ResultSet display = statement.executeQuery("\nSELECT * FROM STUDENT\n");
+		while (display.next()) {
+			System.out.println(display.getInt(1) + "\t" + display.getString(2) + "\t" + display.getString(3) + "\t"
+					+ display.getInt("mobileno") + "\t" + display.getString("location") + "\t" + display.getString(6));
+		}
+		//Delete Record
+		int id = scan.nextInt();
+		int dRecord = 0;
+		String deletequery = "DELETE FROM STUDENT WHERE ID=?";
+		PreparedStatement delete = con.prepareStatement(deletequery);
+		delete.setInt(1, id);
+		dRecord = delete.executeUpdate();
+		if (dRecord > 0)
+			System.out.println("Record Delete");
+		
+		//Update Record
+		id = scan.nextInt();
+		System.out.println("Enter student Address To Update Record:");
+		String address = scan.next();
+		int update = 0;
+		String updatequery = "UPDATE STUDENT SET LOCATION=? WHERE ID=?";
+		PreparedStatement preparedstmt = con.prepareStatement(updatequery);
+		preparedstmt.setString(1, address);
+		preparedstmt.setInt(2, id);
+		update = preparedstmt.executeUpdate();
+		if (update > 0)
+			System.out.println("Record Updated Successfully");
+		con.close();
 
+	}
 }
